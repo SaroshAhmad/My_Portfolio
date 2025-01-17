a737b5845158fbd67afc5f407f7324c776d37fd4
SaroshAhmad <ahmadsarosh07@gmail.com>
2025-01-17 00:45:07 +0000

    Refactor technology tags in Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,13 +13,24 @@ const Experience = () => {
                             </p>
                         </div>
                         <div className="w-full max-w-xl lg:w-3/4">
-                            <h3 className="mb-2 font-semibold">{experience.role} -{" "}
+                            <h3 className="mb-2 font-semibold">
+                                {experience.role} -{" "}
                                 <span className="text-sm text-stone-500">{experience.company}</span>
                             </h3>
                             <p className="mb-4 text-stone-400">{experience.description}</p>
-                            {experience.technologies.map((tech, index) => (<span className="mr-2 mt-4 rounded bg-stone-900 px-2 py-1 text-sm font-medium text-stone-300" key={index}>{tech}</span>
-                            ))}
+                            {/* Flex container for technologies */}
+                            <div className="flex flex-wrap gap-2 mt-4">
+                                {experience.technologies.map((tech, index) => (
+                                    <span
+                                        className="rounded bg-stone-900 px-3 py-1 text-xs md:text-sm font-medium text-stone-300"
+                                        key={index}
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
                         </div>
+
                     </div>
                 ))}
             </div>

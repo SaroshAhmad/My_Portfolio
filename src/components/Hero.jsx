5a8898ef7184806318b0bbb4a647e87a321ad5e2
SaroshAhmad <ahmadsarosh07@gmail.com>
2025-01-17 01:22:40 +0000

    Refactor Contact and Hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,17 @@ import { HERO_CONTENT } from "../constants"
 
 const Hero = () => {
     return (
-        <div className="pb-4 lg:mb-36">
+        <div className="pb-4 lg:mb-36 mt-10">
             <div className="flex flex-wrap lg:flex-row-reverse">
                 <div className="w-full lg:w-1/2">
                     <div className="flex justify-center lg:p-8">
-                        <img src={profilePic} alt="Ravi Kumar" className="border border-stone-900 rounded-3xl" />
+                        <img src={profilePic} alt="Ahmad Sarosh" className="border border-stone-900 rounded-3xl" />
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start mt-10">
-                        <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">
-                            Ahmad Sarosh
+                        <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl mt-0 lg:mt-10">
+                            Ahmad <span className="text-cyan-500">Sarosh</span>
                         </h2>
                         <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</span>
                         <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">

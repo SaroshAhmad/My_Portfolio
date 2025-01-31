759ec2c49aa99dd32582b5956b09cc32f6b1e466
SaroshAhmad <ahmadsarosh07@gmail.com>
2025-01-31 10:20:46 +0000

    Add PostgreSQL icon to Technologies component

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -24,6 +24,8 @@ import { TbBrandFramerMotion } from "react-icons/tb";
 import { SiExpress } from "react-icons/si";
 import { DiIllustrator } from "react-icons/di";
 import { SiRedux } from "react-icons/si";
+import { SiPostgresql } from "react-icons/si";
+
 
 
 const technologiesByCategory = {
@@ -51,6 +53,7 @@ const technologiesByCategory = {
     ],
     "Databases": [
         { icon: <SiMysql />, name: "MySQL", link: "https://www.mysql.com/", color: "text-cyan-600" },
+        { icon: <SiPostgresql />, name: "PostgreSQL", link: "https://www.postgresql.org/", color: "text-blue-600" },
         { icon: <SiMongodb />, name: "MongoDB", link: "https://www.mongodb.com/", color: "text-green-600" },
         { icon: <SiRedis />, name: "Redis", link: "https://redis.io/", color: "text-orange-600" },
     ],
